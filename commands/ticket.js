2732cb7017e0b036441d67df15478531b85e096e
lsminter <lsminteris@gmail.com>
2023-06-15 10:05:11 -0700

    change report to ticket, updated description, now displays user nickname instead of username

--- commands/ticket.js
+++ commands/ticket.js
@@ -2,17 +2,18 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('report')
-		.setDescription('Sends a message to the staff team')
+		.setName('ticket')
+		.setDescription('Sends message to staff team privately.')
 		.addStringOption(option =>
 			option.setName('message')
-				.setDescription('Send a message to the staff without anyone else seeing it.')
+				.setDescription('No one in this channel will see this.')
 				.setRequired(true)),
 	async execute(interaction) {
 
 		const message = interaction.options.getString('message');
 		const channel = interaction.guild.channels.cache.get('748571128687100002');
-		await channel.send(`New report from ${interaction.user.tag}: ${message}`)
+		const commandUser = interaction.guild.members.cache.get(interaction.user.id);
+		await channel.send(`New report from "${commandUser.nickname}": ${message}`)
 		await interaction.reply({content: 'Your report has been sent to the staff team.', ephemeral: true});
 	},
 };
