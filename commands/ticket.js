5cd9bca5c7460d8bd02f55ceece0893e8fe07ea6
lsminter <lsminteris@gmail.com>
2023-06-15 10:15:08 -0700

    added error handling for users without a nickname

--- commands/ticket.js
+++ commands/ticket.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ticket')
+		.setName('report')
 		.setDescription('Sends message to staff team privately.')
 		.addStringOption(option =>
 			option.setName('message')
@@ -11,9 +11,10 @@ module.exports = {
 	async execute(interaction) {
 
 		const message = interaction.options.getString('message');
-		const channel = interaction.guild.channels.cache.get('748571128687100002');
-		const commandUser = interaction.guild.members.cache.get(interaction.user.id);
-		await channel.send(`New report from "${commandUser.nickname}": ${message}`)
+		const channel = interaction.guild.channels.cache.get('938082130200768562');
+		const commandUser = await interaction.guild.members.cache.get(interaction.user.id) ;
+		const commandUserNickname = commandUser.nickname ? commandUser.nickname : commandUser.user.username;
+		await channel.send(`New report from "${commandUserNickname}": ${message}`)
 		await interaction.reply({content: 'Your report has been sent to the staff team.', ephemeral: true});
 	},
 };
